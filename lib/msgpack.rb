ed69aafa9bc0e84b259316f4043d98fbbae2e622
Muga Nishizawa <muga.nishizawa@gmail.com>
2011-09-29 15:16:45 +0900

    changed lib/msgpack.rb

--- lib/msgpack.rb
+++ lib/msgpack.rb
@@ -5,19 +5,32 @@ module MessagePack
     begin
       require 'java'
 
+      # load dependencies of MessagePack for Java
       require 'slf4j-api-1.6.1.jar'
       require 'slf4j-log4j12-1.6.1.jar'
       require 'log4j-1.2.16.jar'
       require 'json-simple-1.1.jar'
       require 'javassist-3.15.0-GA.jar'
+      # load MessagePack for Java
       require 'msgpack-0.6.0-devel.jar'
 
+      # load Java extension of MessagePack for Ruby
       require 'msgpack/msgpack.jar'
-      require 'msgpack/utils'
+
+      # add to_msgpack method in following classes
+      classes = [ Array, Bignum, FalseClass, Fixnum, Float, Hash, NilClass, String, Symbol, TrueClass ]
+      classes.each { |cl|
+        cl.class_eval do
+          def to_msgpack(out = '')
+            return MessagePack.pack(self, out)
+          end
+        end
+      }
     rescue LoadError => e
       raise e
     end
   else
+    # load C extension of MessagePack for Ruby
     require 'msgpack.so'
   end
 end
