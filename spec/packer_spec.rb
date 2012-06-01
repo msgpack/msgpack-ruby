d2ffd44d506968c338450b406a69afcdf0018342
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2012-06-01 12:02:51 -0700

    added spec/packer_spec

---
+++ spec/packer_spec.rb
@@ -0,0 +1,69 @@
+# encoding: ascii-8bit
+require 'spec_helper'
+require 'stringio'
+
+if defined?(Encoding)
+  Encoding.default_internal = 'ASCII-8BIT'
+  Encoding.default_external = 'ASCII-8BIT'
+end
+
+describe Packer do
+  let :packer do
+    Packer.new
+  end
+
+  it 'initialize' do
+    Packer.new
+    Packer.new(nil)
+    Packer.new(StringIO.new)
+    Packer.new({})
+    Packer.new(StringIO.new, {})
+  end
+
+  it 'write' do
+    packer.write([])
+    packer.to_s.should == "\x90"
+  end
+
+  it 'write_nil' do
+    packer.write_nil
+    packer.to_s.should == "\xc0"
+  end
+
+  it 'write_array_header 0' do
+    packer.write_array_header(0)
+    packer.to_s.should == "\x90"
+  end
+
+  it 'write_array_header 1' do
+    packer.write_array_header(1)
+    packer.to_s.should == "\x91"
+  end
+
+  it 'write_map_header 0' do
+    packer.write_map_header(0)
+    packer.to_s.should == "\x80"
+  end
+
+  it 'write_map_header 1' do
+    packer.write_map_header(1)
+    packer.to_s.should == "\x81"
+  end
+
+  it 'flush' do
+    io = StringIO.new
+    pk = Packer.new(io)
+    pk.write_nil
+    pk.flush
+    pk.to_s.should == ''
+    io.string.should == "\xc0"
+  end
+
+  it 'buffer' do
+    o1 = packer.buffer.object_id
+    packer.buffer << 'frsyuki'
+    packer.buffer.to_s.should == 'frsyuki'
+    packer.buffer.object_id.should == o1
+  end
+end
+
