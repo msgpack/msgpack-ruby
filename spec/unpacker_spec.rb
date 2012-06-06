c1fe58e362578a4f77c3d0fd5cb67132874a0f0c
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2012-06-06 11:13:57 -0700

    more test cases for Unpacker

--- spec/unpacker_spec.rb
+++ spec/unpacker_spec.rb
@@ -6,7 +6,53 @@ describe Unpacker do
     Unpacker.new
   end
 
-  # TODO read methods
+  # TODO initialize
+
+  it 'read_array_header succeeds' do
+    unpacker.feed("\x91")
+    unpacker.read_array_header.should == 1
+  end
+
+  it 'read_array_header fails' do
+    unpacker.feed("\x81")
+    lambda {
+      unpacker.read_array_header
+    }.should raise_error(MessagePack::TypeError)
+  end
+
+  it 'read_map_header succeeds' do
+    unpacker.feed("\x81")
+    unpacker.read_map_header.should == 1
+  end
+
+  it 'read_map_header fails' do
+    unpacker.feed("\x91")
+    lambda {
+      unpacker.read_map_header
+    }.should raise_error(MessagePack::TypeError)
+  end
+
+  it 'skip_nil succeeds' do
+    unpacker.feed("\xc0")
+    unpacker.skip_nil.should == true
+  end
+
+  it 'skip_nil fails' do
+    unpacker.feed("\x90")
+    unpacker.skip_nil.should == false
+  end
+
+  # TODO skip methods
+  # TODO feed
+  # TODO each
+  # TODO feed_each
+
+  it 'buffer' do
+    o1 = unpacker.buffer.object_id
+    unpacker.buffer << 'frsyuki'
+    unpacker.buffer.to_s.should == 'frsyuki'
+    unpacker.buffer.object_id.should == o1
+  end
 
   it "gc mark" do
     obj = [1024, {["a","b"]=>["c","d"]}, ["e","f"], "d", 70000, 4.12, 1.5, 1.5, 1.5]
