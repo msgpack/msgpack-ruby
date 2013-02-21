edeed8a0ef57ca9329fd60f899b30b5ca64c76a0
Yuichi Tateno <hotchpotch@gmail.com>
2013-02-21 15:47:58 +0900

    Fix require msgpack/version path

--- ruby/extconf.rb
+++ ruby/extconf.rb
@@ -1,5 +1,5 @@
 require 'mkmf'
-require './version.rb'
+require '../lib/msgpack/version.rb'
 $CFLAGS << %[ -I.. -Wall -O3 -DMESSAGEPACK_VERSION=\\"#{MessagePack::VERSION}\\" -g]
 have_header("ruby/st.h")
 have_header("st.h")
