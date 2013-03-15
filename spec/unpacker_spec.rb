674a0e824e06ea9970237ea197913ffa8e545306
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2013-03-15 11:23:35 -0700

    added more test cases of Unapcker

--- spec/unpacker_spec.rb
+++ spec/unpacker_spec.rb
@@ -6,6 +6,10 @@ describe Unpacker do
     Unpacker.new
   end
 
+  let :packer do
+    Packer.new
+  end
+
   # TODO initialize
 
   it 'read_array_header succeeds' do
@@ -42,16 +46,70 @@ describe Unpacker do
     unpacker.skip_nil.should == false
   end
 
+  it 'skip skips objects' do
+    packer.write(1)
+    packer.write(2)
+    packer.write(3)
+    packer.write(4)
+    packer.write(5)
+
+    unpacker = Unpacker.new(packer.buffer)
+
+    unpacker.read.should == 1
+    unpacker.skip
+    unpacker.read.should == 3
+    unpacker.skip
+    unpacker.read.should == 5
+  end
+
+  it 'read raises EOFError' do
+    lambda {
+      unpacker.read
+    }.should raise_error(EOFError)
+  end
+
+  it 'skip raises EOFError' do
+    lambda {
+      unpacker.skip
+    }.should raise_error(EOFError)
+  end
+
   it 'skip_nil raises EOFError' do
     lambda {
       unpacker.skip_nil
     }.should raise_error(EOFError)
   end
 
-  # TODO skip
-  # TODO feed
-  # TODO each
-  # TODO feed_each
+  let :sample_object do
+    [1024, {["a","b"]=>["c","d"]}, ["e","f"], "d", 70000, 4.12, 1.5, 1.5, 1.5]
+  end
+
+  it 'feed and each continue internal state' do
+    raw = sample_object.to_msgpack.to_s * 4
+    objects = []
+
+    raw.split(//).each do |b|
+      unpacker.feed(b)
+      unpacker.each {|c|
+        objects << c
+      }
+    end
+
+    objects.should == [sample_object] * 4
+  end
+
+  it 'feed_each continues internal state' do
+    raw = sample_object.to_msgpack.to_s * 4
+    objects = []
+
+    raw.split(//).each do |b|
+      unpacker.feed_each(b) {|c|
+        objects << c
+      }
+    end
+
+    objects.should == [sample_object] * 4
+  end
 
   it 'reset clears internal buffer' do
     # 1-element array
@@ -80,11 +138,8 @@ describe Unpacker do
     unpacker.buffer.object_id.should == o1
   end
 
-  it 'raises level stack too deep error' do
-    packer = Packer.new
-    512.times do
-      packer.write_array_header(1)
-    end
+  it 'read raises level stack too deep error' do
+    512.times { packer.write_array_header(1) }
     packer.write(nil)
 
     unpacker = Unpacker.new(packer.buffer)
@@ -93,23 +148,39 @@ describe Unpacker do
     }.should raise_error(MessagePack::StackError)
   end
 
-  it 'raises invalid byte error' do
+  it 'skip raises level stack too deep error' do
+    512.times { packer.write_array_header(1) }
+    packer.write(nil)
+
+    unpacker = Unpacker.new(packer.buffer)
+    lambda {
+      unpacker.skip
+    }.should raise_error(MessagePack::StackError)
+  end
+
+  it 'read raises invalid byte error' do
     unpacker.feed("\xc6")
     lambda {
       unpacker.read
     }.should raise_error(MessagePack::MalformedFormatError)
   end
 
+  it 'skip raises invalid byte error' do
+    unpacker.feed("\xc6")
+    lambda {
+      unpacker.skip
+    }.should raise_error(MessagePack::MalformedFormatError)
+  end
+
   it "gc mark" do
-    obj = [1024, {["a","b"]=>["c","d"]}, ["e","f"], "d", 70000, 4.12, 1.5, 1.5, 1.5]
-    raw = obj.to_msgpack.to_s * 4
+    raw = sample_object.to_msgpack.to_s * 4
 
     n = 0
     raw.split(//).each do |b|
       GC.start
       unpacker.feed_each(b) {|o|
         GC.start
-        o.should == obj
+        o.should == sample_object
         n += 1
       }
       GC.start
