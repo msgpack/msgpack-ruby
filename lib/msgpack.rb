4a2a9d8753083dc5a14b71888cfcb567fa5af0b5
Theo <theo@iconara.net>
2014-07-16 07:55:29 +0200

    Remove an unnecessary semicolon

--- lib/msgpack.rb
+++ lib/msgpack.rb
@@ -3,7 +3,7 @@ require "msgpack/version"
 if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
   require 'jruby'
   require 'msgpack/msgpack.jar'
-  org.msgpack.jruby.MessagePackLibrary.new.load(JRuby.runtime, false);
+  org.msgpack.jruby.MessagePackLibrary.new.load(JRuby.runtime, false)
 else
   begin
     require "msgpack/#{RUBY_VERSION[/\d+.\d+/]}/msgpack"
