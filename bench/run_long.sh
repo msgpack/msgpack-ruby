ad0e82b184d90e20dee1fde24111ccc2f0b49044
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-03-05 14:56:12 +0900

    fix sar command line

--- bench/run_long.sh
+++ bench/run_long.sh
@@ -7,13 +7,13 @@
 
 echo "pack log long"
 viiite report --regroup bench,threads bench/pack_log_long.rb &
-sar -r 60 600 > pack_log_long.sar &
+sar -o pack_log_long.sar -r 60 600 > /dev/null 2>&1 &
 
 sleep 36060 # 60*60 * 5[threads] * 2[bench] + 60 (cool down)
 
 echo "unpack log long"
 viiite report --regroup bench,threads bench/unpack_log_long.rb &
-sar -r 60 600 > unpack_log_long.sar & &
+sar -o unpack_log_long.sar -r 60 600 > /dev/null 2>&1 &
 
 sleep 36060 # 60*60 * 5[threads] * 2[bench] + 60 (cool down)
 
