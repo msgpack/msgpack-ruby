f3d00b405a3a5c3a7a5225b8f88ce1857187bcc3
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-05-21 11:40:06 +0900

    remove useless util script

--- cross-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/msgpack-ruby
-git remote add onetime $MSGPACK_REPO
-git checkout master
-git pull onetime $BUILD_BRANCH
-git reset --hard $BUILD_POSITION
-bundle install
-bundle rake cross native gem
