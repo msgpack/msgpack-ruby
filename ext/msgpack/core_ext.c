3bb67ee63a0baba73fd534fbe16466377bfa2d4f
NARUSE, Yui <naruse@airemix.jp>
2016-06-28 14:25:03 +0900

    Use RUBY_INTEGER_UNIFICATION

--- ext/msgpack/core_ext.c
+++ ext/msgpack/core_ext.c
@@ -143,14 +143,12 @@ void MessagePack_core_ext_module_init()
     rb_define_method(rb_cNilClass,   "to_msgpack", NilClass_to_msgpack, -1);
     rb_define_method(rb_cTrueClass,  "to_msgpack", TrueClass_to_msgpack, -1);
     rb_define_method(rb_cFalseClass, "to_msgpack", FalseClass_to_msgpack, -1);
-    if (rb_cFixnum == rb_cBignum) {
-        // MRI Ruby >= 2.4 unified Fixnum and Bignum into Integer since Feature #12005
-        // https://github.com/ruby/ruby/commit/f9727c12cc8fbc5f752f5983be1f14bb976e5a13
-        rb_define_method(rb_cFixnum, "to_msgpack", Integer_to_msgpack, -1);
-    } else {
-        rb_define_method(rb_cFixnum, "to_msgpack", Fixnum_to_msgpack, -1);
-        rb_define_method(rb_cBignum, "to_msgpack", Bignum_to_msgpack, -1);
-    }
+#ifdef RUBY_INTEGER_UNIFICATION
+    rb_define_method(rb_cInteger, "to_msgpack", Integer_to_msgpack, -1);
+#else
+    rb_define_method(rb_cFixnum, "to_msgpack", Fixnum_to_msgpack, -1);
+    rb_define_method(rb_cBignum, "to_msgpack", Bignum_to_msgpack, -1);
+#endif
     rb_define_method(rb_cFloat,  "to_msgpack", Float_to_msgpack, -1);
     rb_define_method(rb_cString, "to_msgpack", String_to_msgpack, -1);
     rb_define_method(rb_cArray,  "to_msgpack", Array_to_msgpack, -1);
