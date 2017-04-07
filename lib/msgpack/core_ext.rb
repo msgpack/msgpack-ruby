b1f6ce82b5d64a58854db10baaeb4dcd299a5d10
Aaron Patterson <aaron.patterson@gmail.com>
2017-04-07 16:20:17 -0700

    change to private methods

--- lib/msgpack/core_ext.rb
+++ lib/msgpack/core_ext.rb
@@ -3,7 +3,7 @@ module MessagePack
     def to_msgpack(packer_or_io = nil)
       if packer_or_io
         if packer_or_io.is_a?(MessagePack::Packer)
-          _to_msgpack packer_or_io
+          to_msgpack_with_packer packer_or_io
         else
           MessagePack.pack(self, packer_or_io)
         end
@@ -17,7 +17,8 @@ end
 class NilClass
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_nil
     packer
   end
@@ -26,7 +27,8 @@ end
 class TrueClass
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_true
     packer
   end
@@ -35,7 +37,8 @@ end
 class FalseClass
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_false
     packer
   end
@@ -44,7 +47,8 @@ end
 class Float
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_float self
     packer
   end
@@ -53,7 +57,8 @@ end
 class String
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_string self
     packer
   end
@@ -62,7 +67,8 @@ end
 class Array
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_array self
     packer
   end
@@ -71,7 +77,8 @@ end
 class Hash
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_hash self
     packer
   end
@@ -80,7 +87,8 @@ end
 class Symbol
   include MessagePack::CoreExt
 
-  def _to_msgpack(packer)
+  private
+  def to_msgpack_with_packer(packer)
     packer.write_symbol self
     packer
   end
@@ -90,7 +98,8 @@ if 1.class.name == "Integer"
   class Integer
     include MessagePack::CoreExt
 
-    def _to_msgpack(packer)
+    private
+    def to_msgpack_with_packer(packer)
       packer.write_int self
       packer
     end
@@ -99,7 +108,8 @@ else
   class Fixnum
     include MessagePack::CoreExt
 
-    def _to_msgpack(packer)
+    private
+    def to_msgpack_with_packer(packer)
       packer.write_int self
       packer
     end
@@ -108,7 +118,8 @@ else
   class Bignum
     include MessagePack::CoreExt
 
-    def _to_msgpack(packer)
+    private
+    def to_msgpack_with_packer(packer)
       packer.write_int self
       packer
     end
@@ -119,7 +130,8 @@ module MessagePack
   class ExtensionValue
     include CoreExt
 
-    def _to_msgpack(packer)
+    private
+    def to_msgpack_with_packer(packer)
       packer.write_extension self
       packer
     end
