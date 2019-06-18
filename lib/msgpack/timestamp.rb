72ba0947c5fdc977746b4d728e02fbca9a4b93cd
FUJI Goro (gfx) <gfuji@cpan.org>
2019-06-18 15:33:43 +0900

    fix timestamp.rb with suggested changes

--- lib/msgpack/timestamp.rb
+++ lib/msgpack/timestamp.rb
@@ -6,23 +6,23 @@ module MessagePack
   class Timestamp # a.k.a. "TimeSpec"
     TYPE = -1 # timestamp extension type
 
-    TIMESTAMP32_MAX_SEC = 0x100000000 # 32-bit unsigned int
-    TIMESTAMP64_MAX_SEC = 0x400000000 # 34-bit unsigned int
+    TIMESTAMP32_MAX_SEC = (1 << 32) - 1
+    TIMESTAMP64_MAX_SEC = (1 << 34) - 1
 
     def self.from_msgpack_ext(data)
       case data.length
       when 4
-        # timestamp32 = (uint32be)
+        # timestamp32 (sec: uint32be)
         sec = data.unpack1('L>')
         new(sec, 0)
       when 8
-        # timestamp64 = (uint32be, uint32be)
+        # timestamp64 (nsec: uint30be, sec: uint34be)
         n, s = data.unpack('L>2')
-        sec = (n & 0x3) * 0x100000000 + s
+        sec = ((n & 0b11) << 32) | s
         nsec = n >> 2
         new(sec, nsec)
       when 12
-        # timestam96 = (uint32be, int64be)
+        # timestam96 (nsec: uint32be, sec: int64be)
         nsec, sec = data.unpack('L>q>')
         new(sec, nsec)
       else
@@ -31,18 +31,18 @@ module MessagePack
     end
 
     def self.to_msgpack_ext(sec, nsec)
-      if sec >= 0 && nsec >= 0 && sec < TIMESTAMP64_MAX_SEC
-        if nsec === 0 && sec < TIMESTAMP32_MAX_SEC
-          # timestamp32 = (uint32be)
+      if sec >= 0 && nsec >= 0 && sec <= TIMESTAMP64_MAX_SEC
+        if nsec === 0 && sec <= TIMESTAMP32_MAX_SEC
+          # timestamp32 = (sec: uint32be)
           [sec].pack('L>')
         else
-          # timestamp64 (uint32be, uint32be)
+          # timestamp64 (nsec: uint30be, sec: uint34be)
           sec_high = sec << 32
           sec_low = sec & 0xffffffff
-          [(nsec << 2) | (sec_high & 0x3), sec_low].pack('L>2')
+          [(nsec << 2) | (sec_high & 0b11), sec_low].pack('L>2')
         end
       else
-        # timestamp96 (uint32be, int64be)
+        # timestamp96 (nsec: uint32be, sec: int64be)
         [nsec, sec].pack('L>q>')
       end
     end
