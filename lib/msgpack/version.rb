14af53dff7e2a26e3f2c42f9edfb2bdf8bd34e80
TAGOMORI Satoshi <tagomoris@gmail.com>
2020-02-05 11:01:35 +0900

    v1.3.3

--- lib/msgpack/version.rb
+++ lib/msgpack/version.rb
@@ -1,5 +1,5 @@
 module MessagePack
-  VERSION = "1.3.2"
+  VERSION = "1.3.3"
 
   # NOTE for msgpack-ruby maintainer:
   # Check these things to release new binaryes for new Ruby versions (especially for Windows):
