1052a8eca4903105a2e33ed1a25faebe7bb85fc8
TAGOMORI Satoshi <tagomoris@gmail.com>
2020-02-05 10:57:37 +0900

    make sure to check the rake-compiler-dock version

--- lib/msgpack/version.rb
+++ lib/msgpack/version.rb
@@ -4,6 +4,7 @@ module MessagePack
   # NOTE for msgpack-ruby maintainer:
   # Check these things to release new binaryes for new Ruby versions (especially for Windows):
   # * versions/supports of rake-compiler & rake-compiler-dock
+  #   https://github.com/rake-compiler/rake-compiler-dock/blob/master/History.md
   # * update RUBY_CC_VERSION in Rakefile
   # * check Ruby dependency of released mswin gem details
 end
