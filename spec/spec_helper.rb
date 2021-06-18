c6623e1d29c7ef8c0e27ea307895e27133bd202b
Jean Boussier <jean.boussier@gmail.com>
2021-06-18 12:58:03 +0200

    Call GC.verify_compaction_references as part of the test suite if available

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,16 @@ end
 
 require 'msgpack'
 
+if GC.respond_to?(:verify_compaction_references)
+  # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+  # move objects around, helping to find object movement bugs.
+  GC.verify_compaction_references(double_heap: true, toward: :empty)
+end
+
+if GC.respond_to?(:auto_compact=)
+  GC.auto_compact = true
+end
+
 def java?
   /java/ =~ RUBY_PLATFORM
 end
