2530410a72a47e96f97de715a7ed2f378c531b2f
Satoshi Moris Tagomori <tagomoris@gmail.com>
2022-05-27 10:52:54 +0900

    v1.5.2

--- lib/msgpack/version.rb
+++ lib/msgpack/version.rb
@@ -1,5 +1,5 @@
 module MessagePack
-  VERSION = "1.5.1"
+  VERSION = "1.5.2"
   # Note for maintainers:
   #  Don't miss building/releasing the JRuby version (rake buld:java)
   #  See "How to build -java rubygems" in README for more details.
