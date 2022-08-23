9a3cc03ef70881a34eafc93d88306e1c150d0a6b
Jean Boussier <jean.boussier@gmail.com>
2022-08-23 09:31:48 +0200

    Release 1.5.6

--- lib/msgpack/version.rb
+++ lib/msgpack/version.rb
@@ -1,5 +1,5 @@
 module MessagePack
-  VERSION = "1.5.5"
+  VERSION = "1.5.6"
   # Note for maintainers:
   #  Don't miss building/releasing the JRuby version (rake buld:java)
   #  See "How to build -java rubygems" in README for more details.
