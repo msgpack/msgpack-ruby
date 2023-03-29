682092ffbb8f7a0d04486168b48b0712555903d0
Jean Boussier <jean.boussier@gmail.com>
2023-03-29 16:36:00 +0200

    extconf.rb: use append_cflags

--- ext/msgpack/extconf.rb
+++ ext/msgpack/extconf.rb
@@ -5,15 +5,18 @@ have_header("st.h")
 have_func("rb_enc_interned_str", "ruby.h") # Ruby 3.0+
 have_func("rb_hash_new_capa", "ruby.h") # Ruby 3.2+
 
-$CFLAGS << " -fvisibility=hidden "
-
-unless RUBY_PLATFORM.include? 'mswin'
-  $CFLAGS << %[ -I.. -Wall -O3 #{RbConfig::CONFIG["debugflags"]} -std=gnu99]
-end
+append_cflags([
+  "-fvisibility=hidden",
+  "-I..",
+  "-Wall",
+  "-O3",
+  "-std=gnu99"
+])
+append_cflags(RbConfig::CONFIG["debugflags"]) if RbConfig::CONFIG["debugflags"]
 
 if RUBY_VERSION.start_with?('3.0.') && RUBY_VERSION <= '3.0.5'
   # https://bugs.ruby-lang.org/issues/18772
-  $CFLAGS << ' -DRB_ENC_INTERNED_STR_NULL_CHECK=1 '
+  append_cflags("-DRB_ENC_INTERNED_STR_NULL_CHECK=1")
 end
 
 # checking if Hash#[]= (rb_hash_aset) dedupes string keys (Ruby 2.6+)
@@ -23,21 +26,21 @@ r = rand.to_s
 h[%W(#{r}).join('')] = :foo
 x[%W(#{r}).join('')] = :foo
 if x.keys[0].equal?(h.keys[0])
-  $CFLAGS << ' -DHASH_ASET_DEDUPE=1 '
+  append_cflags("-DHASH_ASET_DEDUPE=1")
 else
-  $CFLAGS << ' -DHASH_ASET_DEDUPE=0 '
+  append_cflags("-DHASH_ASET_DEDUPE=0")
 end
 
 # checking if String#-@ (str_uminus) directly interns frozen strings... ' (Ruby 3.0+)
 begin
   s = rand.to_s.freeze
   if (-s).equal?(s) && (-s.dup).equal?(s)
-    $CFLAGS << ' -DSTR_UMINUS_DEDUPE_FROZEN=1 '
+    append_cflags("-DSTR_UMINUS_DEDUPE_FROZEN=1")
   else
-    $CFLAGS << ' -DSTR_UMINUS_DEDUPE_FROZEN=0 '
+    append_cflags("-DSTR_UMINUS_DEDUPE_FROZEN=0")
   end
 rescue NoMethodError
-  $CFLAGS << ' -DSTR_UMINUS_DEDUPE_FROZEN=0 '
+  append_cflags("-DSTR_UMINUS_DEDUPE_FROZEN=0")
 end
 
 if warnflags = CONFIG['warnflags']
