6bbaa97600430c438675540e1f970d61ce5ccd9e
Jean Boussier <jean.boussier@gmail.com>
2024-10-04 10:49:43 -0400

    Release 1.7.3

--- lib/msgpack/version.rb
+++ lib/msgpack/version.rb
@@ -1,5 +1,5 @@
 module MessagePack
-  VERSION = "1.7.2"
+  VERSION = "1.7.3"
   # Note for maintainers:
   #  Don't miss building/releasing the JRuby version (rake buld:java)
   #  See "How to build -java rubygems" in README for more details.
