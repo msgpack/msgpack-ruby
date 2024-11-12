1978f5b7042f47152f10dfe6a562ad04f461ad1c
Jean Boussier <jean.boussier@gmail.com>
2024-11-12 15:54:42 +0100

    Remove outdated JRUBY ifdefs
    This is no longer used since now there's a Java version
    of the gem.

--- ext/msgpack/unpacker_class.c
+++ ext/msgpack/unpacker_class.c
@@ -284,12 +284,6 @@ static VALUE Unpacker_each_impl(VALUE self)
             raise_unpacker_error(uk, r);
         }
         VALUE v = msgpack_unpacker_get_last_object(uk);
-#ifdef JRUBY
-        /* TODO JRuby's rb_yield behaves differently from Ruby 1.9.3 or Rubinius. */
-        if(rb_type(v) == T_ARRAY) {
-            v = rb_ary_new3(1, v);
-        }
-#endif
         rb_yield(v);
     }
 }
