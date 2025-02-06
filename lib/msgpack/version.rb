e4305b4b8a2506422bc78b6fab624907eb7e74a5
Jean Boussier <jean.boussier@gmail.com>
2025-02-06 09:23:42 +0100

    Release 1.8.0

--- lib/msgpack/version.rb
+++ lib/msgpack/version.rb
@@ -1,5 +1,5 @@
 module MessagePack
-  VERSION = "1.7.5"
+  VERSION = "1.8.0"
   # Note for maintainers:
   #  Don't miss building/releasing the JRuby version (rake buld:java)
   #  See "How to build -java rubygems" in README for more details.
